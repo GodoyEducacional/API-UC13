7611d33953e172410d036add3575559f577b12ba
Godoy <trabalhocdc@gmail.com>
2025-01-22 17:13:57 -0300

    Aula 22-01-2025

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+import carros2025 from "./tabelaCarros.js";
+
 // Importa o modulo do Express
 import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+app.get('/', (requisicao, resposta) => {
+    resposta.status(200).send(carros2025); // Retorna lista de carro com status 200
+});
+
 // define a porta do servidor
 app.listen(3000, () => {
-    console.log("servidor rodando na porta 3000")
+  console.log("servidor rodando na porta 3000");
 });
 
 // Executa o app
-// node app.js
+// node app.js ou ./app.js
