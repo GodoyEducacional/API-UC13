1777a5218c00b03d8f690f1a9e99f678c112961d
GABRIEL SANTOS DE GODOY <gabriel.sgodoy1@senacsp.edu.br>
2025-02-03 17:18:00 -0300

    Aula 03-02-2025

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 
+// Importa as rotas
+import carroRoutes from "./routes/carroRoutes.js";
+
 const app = express();
 
-// Permite o Express entender .JSON
+// Permite o Express ler .JSON
 app.use(express.json());
+
+// Chama as rotas de carros (Esturutra completa do código)
+app.use("/carros", carroRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor rodando na porta 3000");
